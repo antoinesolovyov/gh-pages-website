4d00a644b5687304fd2d7244623192f849b9291e
antoinesolovyov <antoinesolovyov@gmail.com>
2020-02-03 13:05:18 +0300

    Updates

---
+++ precache-manifest.be3bfdb2ed104057f00575bf35172921.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c326204a692b2eb82ec16f33ea9417",
+    "url": "/gh-pages-website/index.html"
+  },
+  {
+    "revision": "49e781adbe52ed819b86",
+    "url": "/gh-pages-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "04a6701609bef9f8e56f",
+    "url": "/gh-pages-website/static/js/2.c4a309c4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gh-pages-website/static/js/2.c4a309c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e781adbe52ed819b86",
+    "url": "/gh-pages-website/static/js/main.49121f74.chunk.js"
+  },
+  {
+    "revision": "56dfdf9f1b6ed8bd972a",
+    "url": "/gh-pages-website/static/js/runtime-main.ab8180fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
